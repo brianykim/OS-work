14ec0908b0bf0d107741f090e2dd605613600fa2
Brian Kim <byk.briankim@gmail.com>
2013-06-03 16:39:05 -0700

    6/3/13 work

---
+++ os_api/_posts/2013-5-28-consumer.markdown
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "Consumer"
+categories: jekyll update
+classes:
+ - name: Android
+ - name: IOS
+---
+
+#Hi, I'm a header!
+HERE IS THE BODY OF THE MARKDOWN DOCUMENT
--- os_api/_site/assets/javascripts/script.js
+++ os_api/_site/assets/javascripts/script.js
@@ -1,7 +1,26 @@
-       var visible=false;
-       $(document).ready(function(){
-        $(".link").click(function(){
-            $(".classes").toggle();
-        });        
-       });
+  	/*function showClasses(elid){
+  		if(elid.className="link")
+  		{
+  			elid.className="clickedlink";
+  			document.getElementById("classes").slideDown("slow");
+  		}
+  		else
+  		{
+  			return;
+  		}
+  	}*/
+  	
+  	$(document).ready(function(){
+  	    $(".link").click(function(e){
+
+  	    	e.preventDefault();
+  	    	$(".posts").slideDown("slow");
+  	    	
+  	    });
+  		                    
+    });       
+	
+    //document.getElementByID("someID").className = "asdf"; 
+    //name.id="fruit"; alert(document.URL);
+
            	 
